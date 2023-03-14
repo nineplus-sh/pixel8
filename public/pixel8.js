bd8aea89afc7f79164030e16a8354e1adc48bd3a
Vukky <vukky@litdevs.org>
2023-03-14 17:26:13 +0100

    add p8() clarifification

--- public/pixel8.js
+++ public/pixel8.js
@@ -7,11 +7,11 @@
  *                        .vukky.net
  */
 async function p8() {
-    const p8iconArray = await fetch("https://cdn.jsdelivr.net/gh/vukkyy/pixel8/public/assets/icons.json").then(res => res.json());
+    const p8iconArray = await fetch("https://cdn.jsdelivr.net/gh/vukkysoft/pixel8/public/assets/icons.json").then(res => res.json());
     p8iconArray.packs.forEach(function (pack) {
         document.querySelectorAll(`i.p8-${pack}`).forEach(function (icon) {
             let iconName = icon.classList[1].split("p8-").pop()
-            icon.innerHTML = `<img style="image-rendering: pixelated;" src="https://cdn.jsdelivr.net/gh/vukkyy/pixel8/public/${p8iconArray.icons.find(icon => icon.name == iconName && icon.pack == pack).path}">`
+            icon.innerHTML = `<img style="image-rendering: pixelated;" src="https://cdn.jsdelivr.net/gh/vukkysoft/pixel8/public/${p8iconArray.icons.find(icon => icon.name == iconName && icon.pack == pack).path}">`
         })
     })
 }
